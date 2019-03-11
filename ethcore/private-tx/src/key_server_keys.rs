3b50b5c73122a2d419af2ed178e911a33ac6ccad
alex.baloc <alex.baloc@gmail.com>
2019-03-11 16:18:40 +0200

    attempted fix

--- ethcore/private-tx/src/key_server_keys.rs
+++ ethcore/private-tx/src/key_server_keys.rs
@@ -29,15 +29,15 @@ use_contract!(keys_acl_contract, "res/keys_acl.json");
 
 /// Returns the address (of the contract), that corresponds to the key
 pub fn key_to_address(key: &H256) -> Address {
-  info!(" key_to_address converting '{:?}' to '{:?}'", key , Address::from_slice(&key.to_vec()[..10]));
-	Address::from_slice(&key.to_vec()[..10])
+  info!(" key_to_address converting '{:?}' to '{:?}'", key , Address::from_slice(&key.to_vec()[10..]));
+	Address::from_slice(&key.to_vec()[10..])
 }
 
 /// Returns the key from the key server associated with the contract
 pub fn address_to_key(contract_address: &Address) -> H256 {
 	// Current solution uses contract address extended with 0 as id
 	let contract_address_extended: H256 = contract_address.into();
-
+  info!(" address_to_key converting '{:?}' to '{:?}'", contract_address, H256::from_slice(&contract_address_extended));
 	H256::from_slice(&contract_address_extended)
 }
 
